4f2c2f78ac6ef1d792cc1b5de9ba62ed8d903625
maricosi <marisaisimoes@gmail.com>
2015-10-01 16:41:38 +0100

    bug date

--- recruitment-hr-presentation/src/main/webapp/resources/js/calendar.js
+++ recruitment-hr-presentation/src/main/webapp/resources/js/calendar.js
@@ -14,3 +14,28 @@ thisCalendar.datepicker('setDate', new Date(year, month, 1));
 		});
 	});
 });
+
+$(document).ready(function()
+		{   
+		    $(".monthPicker").datepicker({
+		        dateFormat: 'MM yy',
+		        changeMonth: true,
+		        changeYear: true,
+		        showButtonPanel: true,
+
+		        onClose: function(dateText, inst) {
+		            var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+		            var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+		            $(this).val($.datepicker.formatDate('MM yy', new Date(year, month, 1)));
+		        }
+		    });
+
+		    $(".monthPicker").focus(function () {
+		        $(".ui-datepicker-calendar").hide();
+		        $("#ui-datepicker-div").position({
+		            my: "center top",
+		            at: "center bottom",
+		            of: $(this)
+		        });
+		    });
+		});
